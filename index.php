f533c15d85465413189f88af405873c6943ef633
Mario <mario_font@icloud.com>
2016-10-24 09:33:19 +0200

    Final version

--- index.php
+++ index.php
@@ -1,31 +1,75 @@
-// PHP-Login by @mariofont
-
 <?php
 
-  $username = "Steve";
-  $password = "Chocolate";
+  session_start(); /* Starts the session */
 
+  if(!isset($_SESSION['UserData']['Username'])){ /* Redirects user to Login.php if not logged in */
+	   header("location:login.php");
+	  exit;
+  }
 ?>
 
-
-
-
-
-
+<!-- Show password protected content down here -->
 
 <!DOCTYPE html>
 <html>
   <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>Logged in</title>
   </head>
   <body>
-    <h1>Hello</h1>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">PHP Login by @mariofont</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Status: logged in</h1>
+        <p class="lead">And just like that you've created your first password protected area with PHP, HTML and a little bit of CSS. Change the username and password in the array for your own and give it a try!</p>
+        <p><a class="btn btn-lg btn-success" href="logout.php" role="button">Log out</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Subheading</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Subheading</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h4>Subheading</h4>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; Mario Font 2016</p>
+      </footer>
+
+    </div>
+    
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
---
+++ login.php
@@ -0,0 +1,69 @@
+<!-- HTML code for Bootstrap framework and form design -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/signin.css">
+    <title>Sign in</title>
+  </head>
+  <body>
+    <div class="container">
+      <form action="" method="post" name="Login_Form" class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputUsername" class="sr-only">Username</label>
+        <input name="Username" type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input name="Password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button name="Submit" value="Login" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+
+<?php
+
+  session_start(); /* Starts the session */
+
+	/* Check Login form submitted */
+	if(isset($_POST['Submit'])){
+		/* Define username and associated password array */
+		$logins = array('Steve' => 'Choco2016','Alex' => 'Ice2017');
+
+		/* Check and assign submitted Username and Password to new variable */
+		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+		/* Check Username and Password existence in defined array */
+		if (isset($logins[$Username]) && $logins[$Username] == $Password){
+			/* Success: Set session variables and redirect to Protected page  */
+			$_SESSION['UserData']['Username']=$logins[$Username];
+			header("location:index.php");
+			exit;
+		} else {
+			?>
+        <!-- Show an error alert -->
+        &nbsp;
+        <div class="alert alert-danger alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <strong>Warning!</strong> Incorrect information.
+        </div>
+      <?php
+		}
+	}
+?>
+
+        </form>
+      </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
