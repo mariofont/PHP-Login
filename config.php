f3052da9117647cf17971fdd773f1741a668b1ec
calebrw <calebrw@gmail.com>
2016-10-25 22:13:36 -0500

    Cleanup some security issues.

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Define username and password */
+$Username = "Steve";
+$Password = "Choco2016"; // Raw password
--- login.php
+++ login.php
@@ -1,12 +1,14 @@
 <?php
-  session_start();
+require_once ('config.php'); // For storing username and password.
+
+session_start();
 ?>
 
 <!-- HTML code for Bootstrap framework and form design -->
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,61 +16,56 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/signin.css">
     <title>Sign in</title>
-  </head>
-  <body>
-    <div class="container">
-      <form action="" method="post" name="Login_Form" class="form-signin">
+</head>
+<body>
+<div class="container">
+    <form action="" method="post" name="Login_Form" class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputUsername" class="sr-only">Username</label>
         <input name="Username" type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input name="Password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
         </div>
         <button name="Submit" value="Login" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
-<?php
+        <?php
 
-  /* Check if login form has been submitted */
-	if(isset($_POST['Submit'])){
+        /* Check if login form has been submitted */
+        if(isset($_POST['Submit'])){
 
-    /* Define username and password */
-    $Username = "Steve";
-    $Password = "Choco2016";
+            /* Check if form's username and password matches */
+            if(($_POST['Username'] == $Username) && ($_POST['Password'] == $Password)) {
 
-    /* Check if form's username and password matches */
-    if(($_POST['Username'] == $Username) && ($_POST['Password'] == $Password)) {
+                /* Success: Set session variables and redirect to protected page */
+                $_SESSION['Username'] = $Username;
 
-      /* Success: Set session variables and redirect to protected page */
-      $_SESSION['Username'] = $Username;
-      $_SESSION['Password'] = $Password;
+                $_SESSION['Active'] = true;
+                header("location:index.php");
+                exit;
 
-      $_SESSION['Active'] = true;
-			header("location:index.php");
-			exit;
+            } else {
+                ?>
+                <!-- Show an error alert -->
+                &nbsp;
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Warning!</strong> Incorrect information.
+                </div>
+                <?php
+            }
+        }
+        ?>
 
-    } else {
-			?>
-        <!-- Show an error alert -->
-        &nbsp;
-        <div class="alert alert-danger alert-dismissible" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <strong>Warning!</strong> Incorrect information.
-        </div>
-      <?php
-		}
-	}
-?>
+    </form>
+</div>
 
-        </form>
-      </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
