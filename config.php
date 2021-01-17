cea95cb22721f3af0f4720f0cb0a82ba8118ef10
Mario <mario_font@icloud.com>
2021-01-17 23:26:45 +0100

    Single quotes password fix

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 /* Define username and password */
-$Username = "Steve";
-$Password = "$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK";
+$Username = 'Steve';
+$Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
