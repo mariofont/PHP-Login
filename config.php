d76628ecb8190fdf4d8b0db65c6673fc5040a904
Kartikeya <kudlu2004@gmail.com>
2022-12-09 00:26:15 +0530

    Enabled multiple users entry in config

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
 /* Define username and password */
-$Username = 'Steve';
-$Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
+$users = ['name1', 'name2'];
+$pass = ['12', '123']; // corresponding passwords;
+// $Username = 'Steve';
+// $Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
